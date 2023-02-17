3150409b29c8486410077cbf0b687555afcd9171
lesha <lesha.rudiko@gmail.com>
2023-02-17 21:03:27 +0300

    update

---
+++ diffTask/diffTask.js
@@ -0,0 +1,3 @@
+let namePerson = prompt ("введите имя"); 
+    let result = namePerson == "Артем" ? console.log('директор') : console.log('студент');
+    let result1 = namePerson == "Александр" ? console.log('преподаватель') : console.log('студент');
