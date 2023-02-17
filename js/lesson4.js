2e26a664e80c391104f76cccfbabb7280dc39b1e
lesha <lesha.rudiko@gmail.com>
2023-02-17 20:56:43 +0300

    new lesson

---
+++ js/lesson4.js
@@ -0,0 +1,11 @@
+let getAllServicePrices 
+
+
+let allServicePrices = function(serv1, serv2) {
+    return serv1 + serv2 
+}
+
+getAllServicePrices = allServicePrices(4500, 2200)
+
+
+console.log(getAllServicePrices);
