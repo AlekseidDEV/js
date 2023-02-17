3c86fe9605662d9d05bae9ba42dd7478c56fa4e4
lesha <lesha.rudiko@gmail.com>
2023-02-17 23:46:00 +0300

    less4

--- js/lesson4.js
+++ js/lesson4.js
@@ -1,4 +1,10 @@
+// задание 1
 let getAllServicePrices 
+let screenPrive = 20000;
+let rollBack = 2900;
+let screens = "простые, сложные, интерактивныe";
+
+console.log(screens.split(" , "));
 
 
 let allServicePrices = function(serv1, serv2) {
@@ -7,5 +13,57 @@ let allServicePrices = function(serv1, serv2) {
 
 getAllServicePrices = allServicePrices(4500, 2200)
 
-
 console.log(getAllServicePrices);
+
+
+
+// задание 2
+
+function getFullPrice () {
+    function step(str) {
+        console.log(str);
+    }
+
+    step(getAllServicePrices + screenPrive)
+}
+
+getFullPrice()
+
+// задание 3
+
+let titleText = 'MuThErFucKeR'
+function getTitle(obj) {
+    return titleText[0].toUpperCase() + titleText.slice(1 , 12).toLocaleLowerCase()
+}
+
+console.log(getTitle());
+
+// задание 4
+
+function getServicePercentPrices () {
+    function step(str) {
+        console.log(str);
+    }
+
+    step(getAllServicePrices - rollBack)
+}
+
+getServicePercentPrices()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
