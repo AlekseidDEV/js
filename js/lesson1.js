4a054dc60193a31847adabe046c77cf4754d0948
lesha <lesha.rudiko@gmail.com>
2023-02-21 19:11:27 +0300

    update

--- js/lesson1.js
+++ js/lesson1.js
@@ -1,26 +1,24 @@
 let title = "Somethingveryimpotant";
 let screens = "простые, сложные, интерактивныe";
 let screenPrice = 50;
-let rollback = 29;
+let rollback = 2900;
 let fullPrice = 120000;
 let adaptive = 1 > 2;
 
-    console.log(typeof(title));
-    console.log(typeof(fullPrice));
-    console.log(typeof(adaptive));
-    console.log(screens.length);
-     
-    screenPrice = 50 + "рублей"
-        console.log(screenPrice);
+screenPrice = 50 + "рублей"
+fullPrice = 120000 + " $"
+screenPrice = 50
+fullPrice = 120000
 
-        fullPrice = 120000 + "$"
-            console.log(fullPrice);
 
-            console.log(screens.toUpperCase());
-            console.log(screens.split("е"));
-            console.log(fullPrice);
-    
-            screenPrice = 50
-            fullPrice = 120000
 
-            console.log(fullPrice*(rollback/100));
+console.log(fullPrice);
+console.log(screenPrice);
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log(screens.toUpperCase());
+console.log(screens.split("е"));
+console.log(fullPrice);
+console.log(fullPrice*(rollback/100));
