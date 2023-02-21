3dbbbd3744c00eda14daf3c7d8b57465102489cd
lesha <lesha.rudiko@gmail.com>
2023-02-21 19:52:19 +0300

    update

--- diffTask/diffTask.js
+++ diffTask/diffTask.js
@@ -1,3 +1,3 @@
-let namePerson = prompt ("введите имя"); 
-    let result = namePerson == "Артем" ? console.log('директор') : console.log('студент');
-    let result1 = namePerson == "Александр" ? console.log('преподаватель') : console.log('студент');
+// let namePerson = prompt ("введите имя"); 
+//     let result = namePerson == "Артем" ? console.log('директор') : console.log('студент');
+//     let result1 = namePerson == "Александр" ? console.log('преподаватель') : console.log('студент');
--- js/lesson2.js
+++ js/lesson2.js
@@ -1,25 +1,58 @@
+let title = prompt ('как называется твой проект ?');
+let screens = prompt ('какие типы экранов нужно разработать?');
 let rollback = 2900;
-
-// задание 3
-
-//  let title = prompt("как называется твой проект?")
-//  console.log(title);
-
-// задание 4
-
-// let screens = prompt('какие типы экранов нужно разработать?', 'Простые, сложные, Интерактивные')
-// console.log(screens);
-
-// задание 5
-
-// let screenPrice = prompt("сколько будет стоить данная работа?")
-// console.log(screenPrice);
+let fullPrice = 120000;
+let adaptive = confirm('адаптив нужен?');
 
 
-// задание 6
+let screenPrice = +prompt("сколько будет стоить данная работа?")
+console.log(screenPrice);
 
-// let adaptive = confirm("нужен адаптив?")
-// console.log(adaptive);
+let service = prompt('какой дополнительный тип услуги нужен?' , 'услуга 1 или услуга 2?')
+console.log(service);
+if (service == "услуга 1") {
+    let servPrice1 = +prompt("сколько будет стоить услуга 1?")
+    console.log(servPrice1);
+let fullPrice = screenPrice+servPrice1-rollback
+    console.log("итоговая сумма с учетом отката посреднику:");
+    console.log(fullPrice);
+
+} else if (service == "услуга 2") {
+let servPrice2 = +prompt("сколько будет стоить услуга 2? ")
+    console.log(servPrice2);
+let fullPrice = screenPrice+servPrice2-rollback
+    console.log("итоговая сумма с учетом отката посреднику:");
+    console.log(fullPrice);
+} else {
+    console.log("ты чо дурак? нормально напиши!");
+}  
+
+let saleFull = +prompt('введите итоговую стоимость, для получения скидки')
+    switch (true) {
+        case saleFull < 0:
+            console.log("ты чо бальной?");
+            break
+        case saleFull > 0 && saleFull < 15000:
+            console.log("скидка не предусмотрена");
+            break
+        case saleFull > 15000 && saleFull <30000:
+            console.log("скидка 5%");
+            break
+        case saleFull > 30000:
+            console.log("скидка 10%");
+            break
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -43,40 +76,3 @@ let rollback = 2900;
 
 // // задание 8 + 9 + 10
 
-// let screenPrice = +prompt("сколько будет стоить данная работа?")
-// console.log(screenPrice);
-
-// let service = prompt('какой дополнительный тип услуги нужен?' , 'услуга 1 или услуга 2?')
-// console.log(service);
-// if (service == "услуга 1") {
-//     let servPrice1 = +prompt("сколько будет стоить услуга 1?")
-//     console.log(servPrice1);
-// let fullPrice = screenPrice+servPrice1-rollback
-//     console.log("итоговая сумма с учетом отката посреднику:");
-//     console.log(fullPrice);
-
-// } else if (service == "услуга 2") {
-// let servPrice2 = +prompt("сколько будет стоить услуга 2? ")
-//     console.log(servPrice2);
-// let fullPrice = screenPrice+servPrice2-rollback
-//     console.log("итоговая сумма с учетом отката посреднику:");
-//     console.log(fullPrice);
-// } else {
-//     console.log("ты чо дурак? нормально напиши!");
-// }  
-
-// let saleFull = +prompt('введите итоговую стоимость, для получения скидки')
-//     switch (true) {
-//         case saleFull < 0:
-//             console.log("ты чо бальной?");
-//             break
-//         case saleFull > 0 && saleFull < 15000:
-//             console.log("скидка не предусмотрена");
-//             break
-//         case saleFull > 15000 && saleFull <30000:
-//             console.log("скидка 5%");
-//             break
-//         case saleFull > 30000:
-//             console.log("скидка 10%");
-//             break
-//     }
