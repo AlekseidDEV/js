23890997e03570460b74a30ab182a9b94a41b186
lesha <lesha.rudiko@gmail.com>
2023-02-21 16:31:59 +0300

    up

---
+++ js/lesson5.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let title = prompt ("Как называется ваш проект?")
+let screns = prompt ("Какие типы экранов нужно разработать?")
+let screenPrice = prompt ("сколько будет стоить данная работа?")
+let adaptive = prompt ("нужен ли адаптив на сайте?")
+
+let service1 = prompt ("Какой дополнительный тип услуг  нужен?")
+let servicePrice1 = prompt ("сколько это будет стоить?")
+let service2 = prompt ("Какой дополнительный тип услуг  нужен ?")
+let servicePrice2 = prompt ("сколько это будет стоить?")
+
+let rollBack = 2900;
+let getAllServicePrices 
+
+
