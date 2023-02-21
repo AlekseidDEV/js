79fbb86134ded1667c33e97cd04a720247249139
lesha <lesha.rudiko@gmail.com>
2023-02-21 17:44:00 +0300

    commit

--- js/lesson5.js
+++ js/lesson5.js
@@ -1,16 +1,38 @@
 'use strict';
 
-let title = prompt ("Как называется ваш проект?")
-let screns = prompt ("Какие типы экранов нужно разработать?")
-let screenPrice = prompt ("сколько будет стоить данная работа?")
-let adaptive = prompt ("нужен ли адаптив на сайте?")
+// let title = prompt ("Как называется ваш проект?")
+// let screns = prompt ("Какие типы экранов нужно разработать?")
+// let screenPrice = prompt ("сколько будет стоить данная работа?")
+// let adaptive = prompt ("нужен ли адаптив на сайте?")
 
-let service1 = prompt ("Какой дополнительный тип услуг  нужен?")
-let servicePrice1 = prompt ("сколько это будет стоить?")
-let service2 = prompt ("Какой дополнительный тип услуг  нужен ?")
-let servicePrice2 = prompt ("сколько это будет стоить?")
+// let service1 = prompt ("Какой дополнительный тип услуг  нужен?")
+// let servicePrice1 = prompt ("сколько это будет стоить?")
+// let service2 = prompt ("Какой дополнительный тип услуг  нужен ?")
+// let servicePrice2 = prompt ("сколько это будет стоить?")
 
-let rollBack = 2900;
-let getAllServicePrices 
+// let rollBack = 2900;
+// let allServicePrice
+// let fullPrice
+// let servicePercentPrice
+
+// const getAllServicePrice = function() {
+//     return servicePrice1 + servicePrice2
+// }
+
+// const showTypeOf = function (variable) {
+//     console.log(variable, typeof variable);
+// }
+
+// const getFullPrice = function() {
+//     return fullPrice - (fullPrice *(rollBack / 100))
+// }
+
+// const getTitle = function () {
+//     return title[0].toUpperCase() + title.substring(1).toLocaleLowerCase()
+// }
+
+// const getRollbackMessege = function (price)
+
+
 
 
